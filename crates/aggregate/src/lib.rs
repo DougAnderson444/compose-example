464d1e8d9e8be8bf180f392b9fe57524d6982071
Doug Anderson444 <douganderson444@gmail.com>
2023-12-30 00:23:09 -0400

    use imports

--- crates/aggregate/src/lib.rs
+++ crates/aggregate/src/lib.rs
@@ -1,15 +1,17 @@
 cargo_component_bindings::generate!();
 
 use bindings::exports::aggregate::aggregate::controls::Guest;
+use bindings::increment::increment::increase::up;
+use bindings::shower::show::display::view;
 
 struct Component;
 
 impl Guest for Component {
     fn more() {
-        println!("More!");
+        up();
     }
 
     fn inspect() -> u32 {
-        42
+        view()
     }
 }
