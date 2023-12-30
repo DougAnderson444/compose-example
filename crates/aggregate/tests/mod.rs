e86111a9fac929b103741ad2de10a5a598e9f8da
Doug Anderson444 <douganderson444@gmail.com>
2023-12-30 00:50:01 -0400

    add test

---
+++ crates/aggregate/tests/mod.rs
@@ -0,0 +1,132 @@
+mod bindgen {
+    wasmtime::component::bindgen!("example"); // name of the world in the .wit file
+}
+
+use std::path::{Path, PathBuf};
+use wasmtime::component::{Component, Linker};
+use wasmtime::{Config, Engine, Store};
+use wasmtime_wasi::preview2::{Table, WasiCtx, WasiCtxBuilder, WasiView};
+
+// use bindgen::exports::aggregate::aggregate::controls::{inspect, more};
+
+struct MyCtx {
+    wasi_ctx: Context,
+}
+
+struct Context {
+    table: Table,
+    wasi: WasiCtx,
+}
+impl WasiView for MyCtx {
+    fn table(&self) -> &Table {
+        &self.wasi_ctx.table
+    }
+    fn table_mut(&mut self) -> &mut Table {
+        &mut self.wasi_ctx.table
+    }
+    fn ctx(&self) -> &WasiCtx {
+        &self.wasi_ctx.wasi
+    }
+    fn ctx_mut(&mut self) -> &mut WasiCtx {
+        &mut self.wasi_ctx.wasi
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum TestError {
+    /// From String
+    #[error("Error message {0}")]
+    Stringified(String),
+
+    /// From Wasmtime
+    #[error("Wasmtime: {0}")]
+    Wasmtime(#[from] wasmtime::Error),
+
+    /// From VarError
+    #[error("VarError: {0}")]
+    VarError(#[from] std::env::VarError),
+
+    /// From io
+    #[error("IO: {0}")]
+    Io(#[from] std::io::Error),
+}
+
+impl From<String> for TestError {
+    fn from(s: String) -> Self {
+        TestError::Stringified(s)
+    }
+}
+
+/// Utility function to get the workspace dir
+pub fn workspace_dir() -> PathBuf {
+    let output = std::process::Command::new(env!("CARGO"))
+        .arg("locate-project")
+        .arg("--workspace")
+        .arg("--message-format=plain")
+        .output()
+        .unwrap()
+        .stdout;
+    let cargo_path = Path::new(std::str::from_utf8(&output).unwrap().trim());
+    cargo_path.parent().unwrap().to_path_buf()
+}
+
+#[cfg(test)]
+mod aggregate_example_tests {
+
+    use super::*;
+
+    #[test]
+    fn test_initial_load() -> wasmtime::Result<(), TestError> {
+        // get the target/wasm32-wasi/debug/CARGO_PKG_NAME.wasm file
+        let pkg_name = std::env::var("CARGO_PKG_NAME")?.replace('-', "_");
+        let workspace = workspace_dir();
+        // the .wasm f
+        // ile is in the root of the workspace
+        let wasm_path = format!("{}.wasm", pkg_name);
+        let wasm_path = workspace.join(wasm_path);
+
+        let mut config = Config::new();
+        config.cache_config_load_default()?;
+        config.wasm_backtrace_details(wasmtime::WasmBacktraceDetails::Enable);
+        config.wasm_component_model(true);
+
+        let engine = Engine::new(&config)?;
+        let component = Component::from_file(&engine, &wasm_path)?;
+
+        let mut linker = Linker::new(&engine);
+
+        // link imports like get_seed to our instantiation
+        // bindgen::Example::add_to_linker(&mut linker, |state: &mut MyCtx| state)?;
+
+        // link the WASI imports to our instantiation
+        wasmtime_wasi::preview2::command::sync::add_to_linker(&mut linker)?;
+
+        let table = Table::new();
+        let wasi: WasiCtx = WasiCtxBuilder::new().inherit_stdout().args(&[""]).build();
+        let state = MyCtx {
+            wasi_ctx: Context { table, wasi },
+        };
+        let mut store = Store::new(&engine, state);
+
+        let (bindings, _) = bindgen::Example::instantiate(&mut store, &component, &linker)?;
+
+        // Use bindings to increment and show the count
+        let count = bindings
+            .aggregate_aggregate_controls()
+            .call_inspect(&mut store)?;
+
+        assert_eq!(count, 0);
+
+        bindings
+            .aggregate_aggregate_controls()
+            .call_more(&mut store)?;
+
+        let count = bindings
+            .aggregate_aggregate_controls()
+            .call_inspect(&mut store)?;
+
+        assert_eq!(count, 1);
+
+        Ok(())
+    }
+}
